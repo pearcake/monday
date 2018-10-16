f0904aacff00c5122b3bc5f637de4465ef453346
Sergey Borisenko <pearcake@users.noreply.github.com>
2018-10-16 19:46:02 +0300

    Update enqueue.php
    Update enqueue.php scripts to have filemtime instead of semver file versions for better browser cache updating.

--- lib/enqueue.php
+++ lib/enqueue.php
@@ -8,9 +8,9 @@ function theme_scripts() {
     wp_enqueue_script('polyfill', 'https://cdn.polyfill.io/v2/polyfill.min.js?features=default,Array.prototype.includes', array('jquery'), null);
   }
 
-  wp_enqueue_script('theme-js', get_stylesheet_directory_uri() . '/assets/dist/js/theme.js', array('jquery'), true);
+  wp_enqueue_script('theme-js', get_stylesheet_directory_uri() . '/assets/dist/js/theme.js', array('jquery'), filemtime( get_theme_file_path('assets/dist/js/theme.js') ), true);
 
-  wp_enqueue_style( 'theme-css', get_stylesheet_directory_uri() . '/assets/dist/css/theme.css', array(), null );
+  wp_enqueue_style( 'theme-css', get_stylesheet_directory_uri() . '/assets/dist/css/theme.css', array(), filemtime( get_theme_file_path('assets/dist/css/theme.css') ) );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
